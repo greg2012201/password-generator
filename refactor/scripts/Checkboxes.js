39274c89eabad9eae31dbcdb119e851b59ab235f
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-04 10:39:23 +0200

    Checboxes.js created

---
+++ refactor/scripts/Checkboxes.js
@@ -0,0 +1,8 @@
+class Checkboxes {
+
+    constructor() {
+        this.numberChecker = document.querySelector('.numberChecker');
+        this.charactersChecker = document.querySelector('.charactersChecker');
+    }
+
+}
