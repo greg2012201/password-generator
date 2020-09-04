9eae1b3ae8135a429965be44d1291c6098f7f742
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-04 10:34:52 +0200

    Data.js created getWords, getNumbers, getSepcialCharacters

---
+++ refactor/scripts/Data.js
@@ -0,0 +1,16 @@
+class Data {
+
+    getWords() {
+        return this.words = ['coś', 'słOwo', 'pieRdoła', 'Alicja', 'dommieszkalny', 'poszedłemdokina', 'Wlazł', 'koteknapłotek', 'Szpital'];
+    }
+    getNumbers() {
+        this.numbers = ['1', '6', '1', '6', '0', '2', '8', '7', '9'];
+
+    }
+
+    getSepcialCharacters() {
+
+        this._inputSpecialCharacters = ['!', '$', '?', '%', '#', '.', '+', '=', '@'];
+    }
+
+}
