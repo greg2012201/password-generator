cd9dffbe1f7290d9dbc358962890214a0acd6245
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-07 10:23:43 +0200

    Draw.js update

--- refactor/scripts/Draw.js
+++ refactor/scripts/Draw.js
@@ -10,11 +10,11 @@ class Draw {
 
         console.log(this.getWords);
     }
-    giveMeRandomNumber() {
+    getRandomNumber() {
 
 
     }
-    giveMeRandomChar() {
+    getRandomChar() {
 
     }
 
