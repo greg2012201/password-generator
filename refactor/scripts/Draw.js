fdc24d426fc99a3417ce2932ccc34eb9da4b7d0e
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-08 09:27:40 +0200

    showNumberOfLetters created in Inputs.js

--- refactor/scripts/Draw.js
+++ refactor/scripts/Draw.js
@@ -29,7 +29,6 @@ class Draw {
 
     }
 
-
     getChosenElements(_password, elements /* numbers, chars */ ) {
 
         const slicedElements = elements.slice(Math.floor(Math.random() * elements.length - 1), elements.length);
@@ -38,8 +37,6 @@ class Draw {
         // osobna metoda musi wyjść w sensie sliced żeby nie kopiować kodu
 
     }
-
-
 
     calculationsWithWords(_password) {
 
--- refactor/scripts/Inputs.js
+++ refactor/scripts/Inputs.js
@@ -1,5 +1,16 @@
 class Inputs {
+    constructor() {
+
+        this.input = document.querySelector('.charBar');
+        this.numberOfLettersSpan = document.querySelector('.numberOfLetters');
+        this.input.addEventListener('input', this.showNumberOfLetters.bind(this))
+
+    }
+
     getInputValue() {
-        return document.querySelector('.charBar').value;
+        return this.input.value;
+    }
+    showNumberOfLetters() {
+        this.numberOfLettersSpan.textContent = this.getInputValue();
     }
 }
