d72a3bb04af32fdec64070c69eb4440389a65695
Grzegorz Dubiel <greg2012201@gmail.com>
2020-09-08 08:47:25 +0200

    Draw.js update getChosenelements created and calculations updated

--- refactor/scripts/Draw.js
+++ refactor/scripts/Draw.js
@@ -3,19 +3,30 @@ class Draw {
         this.getNumbers = getNumbers;
         this.getSpecialChars = getSpecialChars;
         this.getWords = getWords;
-
+        /*  let _input = []; */
     }
 
     calculations(isNumber, isChar) {
+        let _input = [];
+        if (isNumber && isChar) {
+            this.getChosenElements(_input, this.getNumbers);
+            this.getChosenElements(_input, this.getSpecialChars);
+            // to trzeba przerobić w jedną funkcję na 2
+        } else if (isNumber && !isChar) this.getChosenElements(_input, getNumbers);
+        else if (isChar && !isNumber) this.getChosenElements(_input, this.getSpecialChars);
 
-        console.log(this.getWords);
+
+
+
     }
-    getRandomNumber() {
+    getChosenElements(_input, elements /* numbers, chars */ ) {
 
+        const slicedElements = elements.slice(Math.floor(Math.random() * elements.length - 1), elements.length);
+        const singleElement = slicedElements.shift();
+        _input.ushift(singleElement);
+        // osobna metoda musi wyjść w sensie sliced żeby nie kopiować kodu
 
     }
-    getRandomChar() {
 
-    }
 
 }
